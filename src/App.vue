632d3acf642b0fa17a30bee1d9824001ce5c414b
18God <1226343906@qq.com>
2022-07-13 19:31:50 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>主页面</h1>
+    <router-v></router-v>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less"></style>
